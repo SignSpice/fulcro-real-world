3000aa67fe566793c35a59c5297a8e93d54ef2fd
SignSpice <little.alaskan@protonmail.com>
2021-11-29 15:34:46 -0900

    Swap out missing/broken components for generic 'Box'.

--- src/main/app/mobile_ui/native_base.cljs
+++ src/main/app/mobile_ui/native_base.cljs
@@ -1,31 +1,25 @@
 (ns app.mobile-ui.native-base
   "Convenience wrappers for Native Base components"
   (:require
-    [com.fulcrologic.fulcro-native.alpha.components :as c :refer [react-factory]]
-    ["native-base" :refer [Container Header Item Input Icon Button Content
-                           List ListItem CheckBox Left Body Right Label Card CardItem
-                           Form Spinner Tab Tabs]]))
+   [com.fulcrologic.fulcro-native.alpha.components :as c :refer [react-factory]]
+   ["native-base" :refer [NativeBaseProvider Box Input Button Card Spinner]]))
 
-(def ui-body (react-factory Body))
+(def ui-body (react-factory Box))
 (def ui-button (react-factory Button))
 (def ui-card (react-factory Card))
-(def ui-card-item (react-factory CardItem))
-(def ui-checkbox (react-factory CheckBox))
-(def ui-container (react-factory Container))
-(def ui-content (react-factory Content))
-(def ui-form (react-factory Form))
-(def ui-header (react-factory Header))
-(def ui-icon (react-factory Icon))
+(def ui-card-item (react-factory Box))
+(def ui-container (react-factory NativeBaseProvider))
+(def ui-content (react-factory Box))
+(def ui-form (react-factory Box))
+(def ui-header (react-factory Box))
 (def ui-input (c/wrap-text-input (react-factory Input)))
-(def ui-item (react-factory Item))
-(def ui-label (react-factory Label))
-(def ui-left (react-factory Left))
-(def ui-list (react-factory List))
-(def ui-list-item (react-factory ListItem))
-(def ui-right (react-factory Right))
+(def ui-item (react-factory Box))
+(def ui-label (react-factory Box))
+(def ui-left (react-factory Box))
+(def ui-right (react-factory Box))
 (def ui-spinner (react-factory Spinner))
-(def ui-tab (react-factory Tab))
-(def ui-tabs (react-factory Tabs))
+(def ui-tab (react-factory Box))
+(def ui-tabs (react-factory Box))
 
 (defn ui-heading
   "A ui-header with the specified string as the single body element"
